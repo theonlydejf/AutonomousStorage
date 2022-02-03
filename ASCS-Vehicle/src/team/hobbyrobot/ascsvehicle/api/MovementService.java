92a02492b05ac5163f7df03bf1acec40441ab68d
theonlydejf <krcmar.dav@gmail.com>
2022-02-03 18:54:52 +0100

    Fixed movement service

--- ASCS-Vehicle/src/team/hobbyrobot/ascsvehicle/api/MovementService.java
+++ ASCS-Vehicle/src/team/hobbyrobot/ascsvehicle/api/MovementService.java
@@ -1,5 +1,8 @@
 package team.hobbyrobot.ascsvehicle.api;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 import lejos.robotics.RegulatedMotor;
 import lejos.robotics.chassis.Chassis;
 import lejos.robotics.chassis.Wheel;
@@ -14,7 +17,10 @@ import team.hobbyrobot.subos.net.api.Service;
 import team.hobbyrobot.subos.net.api.exceptions.RequestGeneralException;
 import team.hobbyrobot.subos.net.api.exceptions.RequestParamsException;
 import team.hobbyrobot.subos.net.api.exceptions.UnknownRequestException;
+import team.hobbyrobot.tdn.base.TDNArray;
+import team.hobbyrobot.tdn.base.TDNParsers;
 import team.hobbyrobot.tdn.core.TDNRoot;
+import team.hobbyrobot.tdn.core.TDNValue;
 
 // TODO opravit
 public class MovementService implements Service
@@ -35,16 +41,58 @@ public class MovementService implements Service
 	public TDNRoot processRequest(String request, TDNRoot params)
 		throws UnknownRequestException, RequestParamsException, RequestGeneralException
 	{
-		switch (request)
+		/*Method method;
+		try
 		{
-			case "travel":
-				logger.log("travelling...");
-				pilot.travel((float) params.get("distance").value);
-			break;
-
-			default:
-				throw new UnknownRequestException();
+			TDNValue paramTypesTDN = params.get("paramTypes");
+			if(paramTypesTDN == null || !paramTypesTDN.parser().typeKey().equals(TDNParsers.ARRAY.typeKey()))
+				throw new RequestParamsException("correct form of paramTypes doesn't exist", "paramTypes");
+			
+			TDNArray paramTypesRaw = params.get("paramTypes").as();
+			if(!paramTypesRaw.itemParser.typeKey().equals(TDNParsers.STRING.typeKey()))
+				throw new RequestParamsException("correct form of paramTypes doesn't exist", "paramTypes");
+			
+			String[] paramTypesStr = Arrays.copyOf(paramTypesRaw.value, paramTypesRaw.value.length, String[].class);
+			Class<?>[] paramTypes = new Class<?>[paramTypesStr.length];
+			for(int i = 0; i < paramTypes.length; i++)
+				paramTypes[i] = Class.forName(paramTypesStr[i]);
+			
+			method = MovePilot.class.getMethod(request, paramTypes);
 		}
+		catch (SecurityException e)
+		{
+			throw new RequestGeneralException(Logger.getExceptionInfo(e));
+		}
+		catch (NoSuchMethodException e)
+		{
+			throw new UnknownRequestException();
+		}
+		catch (ClassNotFoundException e)
+		{
+			throw new RequestParamsException("Unknown class type: " + Logger.getExceptionInfo(e), "paramTypes");
+		}
+		
+		method.invoke(pilot, )*/
+		
+		try
+		{
+			switch(request)
+			{
+				case "travel":
+					pilot.travel((float)params.get("distance").as(), true);
+					break;
+				case "rotate":
+					pilot.rotate((float)params.get("angle").as(), true);
+					break;
+				default:
+					throw new UnknownRequestException();
+			}			
+		}
+		catch(Exception e)
+		{
+			throw new RequestGeneralException(Logger.getExceptionInfo(e));
+		}
+		
 		return new TDNRoot();
 	}
 
