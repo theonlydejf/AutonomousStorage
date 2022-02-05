ad6bd3bfb129a7603d1514d36eaada3967638da4
Stepan-Muller <stepan.muller@montetrida.cz>
2022-02-05 03:29:37 +0100

    Added TDN to project

---
+++ TransmissionDataNotation/Base/TDNParsers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Team.HobbyRobot.TDN.Core;
+
+namespace Team.HobbyRobot.TDN.Base
+{
+    public static class TDNParsers
+    {
+		public static readonly ITDNTypeParser ARRAY = new ArrayParser();
+		public static readonly ITDNTypeParser BOOLEAN = new BooleanParser();
+		public static readonly ITDNTypeParser FLOAT = new FloatParser();
+		public static readonly ITDNTypeParser INTEGER = new IntegerParser();
+		public static readonly ITDNTypeParser STRING = new StringParser();
+		public static readonly ITDNTypeParser ROOT = new TDNRootParser();
+	}
+}
